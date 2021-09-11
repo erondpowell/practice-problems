ad75a633b147b0e76baea8c86e67aeb336d4c437
Eron Powell <erondpowell@gmail.com>
2021-09-11 18:01:02 -0500

    worked through get/set lectures in OOP module

--- academind_js-beginner-to-adv/10_oop-01-starting-setup/assets/scripts/app.js
+++ academind_js-beginner-to-adv/10_oop-01-starting-setup/assets/scripts/app.js
@@ -1,41 +1,144 @@
-const productList = {
-  products : [
-    {
-      title: "A pillow",
-      imageUrl:
-        "https://images.unsplash.com/photo-1584100936595-c0654b55a2e2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGlsbG93fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=60",
-      price: 19.99,
-      description: "A Soft Pillow",
-    },
-    {
-      title: "A Carpet",
-      imageUrl:
-        "https://images.unsplash.com/photo-1594040226829-7f251ab46d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cnVnfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=60",
-      price: 89.99,
-      description: "A Soft Pillow",
-    },
-  ],
-  render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    productList.className = 'product-list';
-    for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-          <div class="product-item__content">
-            <img src=${prod.imageUrl} alt=${prod.title}>
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        `;
-      prodList.append(prodEl);
-    }
-    renderHook.append(prodList);
-  }
+class Product {
+  // title = "DEFAULT";
+  // imageURL;
+  // description;
+  // price;
 
+  constructor(title, image, description, price) {
+    this.title = title;
+    this.imageURL = image;
+    this.description = description;
+    this.price = price;
+  }
 }
 
-productList.render();
+class ShoppingCart {
+  items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+  }
+
+  get totalAmount() {
+    console.log("logging sum1");
+    console.log(this.items);
+    const sum = this.items.reduce(
+      (prevVal, curItem) => prevVal + curItem.price,
+      0
+    );
+    console.log("logging sum2");
+    console.log(sum);
+    return sum;
+  }
+
+  addProduct(product) {
+    // const updatedItems = [...this.items];
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
+    return cartEl;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageURL}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+        `;
+    console.log(prodEl.innerHTML); //This section works.
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+      "A Soft Pillow",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      "A carpet you might like - or not.",
+      89.99
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
+    // const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    console.log(prodList); // works? Renders a UL of with both products as children
+    return prodList;
+  }
+}
+
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const productListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(productListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
