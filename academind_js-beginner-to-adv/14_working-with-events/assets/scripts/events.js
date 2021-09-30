4f5a4b5a50ce5d38e758783282789d3984948b2b
Eron Powell <erondpowell@gmail.com>
2021-09-30 16:40:40 -0500

    experimented with various event listeners, assigned listeners to multiple elements with a forEach loop, and began to extractinfo from the event object on form submissions

--- academind_js-beginner-to-adv/14_working-with-events/assets/scripts/events.js
+++ academind_js-beginner-to-adv/14_working-with-events/assets/scripts/events.js
@@ -1,24 +1,46 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
+// const d = document.getElementById('main-content');
+// const html = document.createElement('p')
 
-// button.onclick = function() {
-
-// };
-
-const buttonClickHandler = () => {
-  alert('Button was clicked!');
+const buttonClickHandler = (event, v='') => {
+  // console.log(event);
+  // console.log('button click handler 1' + (v.length>0 ? v : '.'));
+  // event.target.disabled = true;
+  // alert('some alert');
+  // console.log(html);
+  // let newClone = html.cloneNode([true]);
+  // newClone.innerText = "You hovered over me";
+  // d.appendChild(newClone);
 };
 
-const anotherButtonClickHandler = () => {
-  console.log('This was clicked!');
-};
+// const anotherButtonClickHandler = () => {
+//   console.log('beans');
+// }
 
-// button.onclick = buttonClickHandler;
-// button.onclick = anotherButtonClickHandler;
 
-const boundFn = buttonClickHandler.bind(this);
+// const boundFn = buttonClickHandler.bind(this, 'wacky');
 
-button.addEventListener('click', boundFn);
+// button.addEventListener('click', boundFn);
 
-setTimeout(() => {
-  button.removeEventListener('click', boundFn);
-}, 2000);
+// setTimeout(()=> {
+//   button.removeEventListener("click", boundFn);
+// }, 2000);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", buttonClickHandler);
+})
+
+
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   console.log(event);
+// })
+
+
+const form = document.querySelector("form");
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event.target.value);
+});
